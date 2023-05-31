1934febb1a3ffcac2ce12f54905b11fea942d96e
pomelo <wxlp0323@gmail.com>
2023-05-31 11:21:21 +0800

    add Date 、 Math  and Calendar example

---
+++ Java/java-code/chap07/section13/CalendarExample.java
@@ -0,0 +1,29 @@
+package chap07.section13;
+
+// chap07/section13/CalendarExample.java
+import java.util.Calendar;
+import java.util.Date;
+public class CalendarExample {
+    public static void main(String[] args) {
+        // 获取当前日期和时间的Calendar对象
+        Calendar calendar = Calendar.getInstance();
+        // 获取年份
+        int year = calendar.get(Calendar.YEAR);
+        System.out.println(year); // 2023
+        // 获取月份（需要注意，月份从0开始计数）
+        int month = calendar.get(Calendar.MONTH) + 1;
+        System.out.println(month); // 5
+        // 获取当前是月份中的第几天
+        int dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
+        System.out.println(dayOfMonth); // 31
+        // 设置日期为指定的年份、月份和日
+        calendar.set(2023, 4, 30);
+        // 在月份上增加1
+        calendar.add(Calendar.MONTH, 1);
+        // 输出修改后的日期
+        System.out.println(calendar.getTime()); // Fri Jun 30 09:36:00 CST 2023
+        // 将Calendar转换为Date对象
+        Date date = calendar.getTime();
+        System.out.println(date); // Fri Jun 30 09:36:00 CST 2023
+    }
+}
---
+++ Java/java-code/chap07/section13/MathExample.java
@@ -0,0 +1,34 @@
+package chap07.section13;
+
+// chap07/section13/MathExample.java
+public class MathExample {
+
+    public static void main(String[] args) {
+        int num = -5;
+        int absNum = Math.abs(num);
+        System.out.println(absNum); // 5
+        double n = 16.0;
+        double sqrtNum = Math.sqrt(n);
+        System.out.println(sqrtNum); // 4.0
+        double base = 2.0;
+        double exponent = 3.0;
+        double result = Math.pow(base, exponent);
+        System.out.println(result); // 8.0
+        int num1 = 10;
+        int num2 = 7;
+        int maxNum = Math.max(num1, num2);
+        System.out.println(maxNum); // 10
+        int minNum = Math.min(num1, num2);
+        System.out.println(minNum); // 7
+        double n1 = 4.2;
+        double ceilNum = Math.ceil(n1);
+        System.out.println(ceilNum); // 5.0
+        double floorNum = Math.floor(n1);
+        System.out.println(floorNum); // 4.0
+        double n2 = 4.6;
+        long roundedNum = Math.round(num);
+        System.out.println(roundedNum); // 5
+        double randomNum = Math.random();
+        System.out.println(randomNum); // 0.0~1.0
+    }
+}
